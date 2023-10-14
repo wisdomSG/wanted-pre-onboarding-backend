42761bca5a14fbe40e148d6e23c5f9b9a67af267
seulgi <asd0927122@gmail.com>
2023-10-15 00:37:08 +0900

    Add : RecruitmentPostService 주석 입력 #1

--- src/main/java/com/seulgi/wantedpreonboardingbackend/recruitmentPost/RecruitmentPostService.java
+++ src/main/java/com/seulgi/wantedpreonboardingbackend/recruitmentPost/RecruitmentPostService.java
@@ -4,21 +4,55 @@ import com.seulgi.wantedpreonboardingbackend.recruitmentPost.dto.RecruitmentPost
 import com.seulgi.wantedpreonboardingbackend.recruitmentPost.dto.RecruitmentPostListResponseDto;
 import com.seulgi.wantedpreonboardingbackend.recruitmentPost.dto.RecruitmentPostRequestDto;
 import com.seulgi.wantedpreonboardingbackend.recruitmentPost.dto.RecruitmentPostResponseDto;
+
 import java.util.List;
-import org.springframework.data.domain.Pageable;
 
 public interface RecruitmentPostService {
-
+  /**
+   * 채용공고 생성
+   * @param requestDto
+   * @return
+   */
   RecruitmentPostResponseDto createRecruitmentPost(RecruitmentPostRequestDto requestDto);
 
+  /**
+   * 채용공고 수정
+   * @param requestDto
+   * @param recruitmentPostId
+   * @return
+   */
   RecruitmentPostResponseDto updateRecruitmentPost(RecruitmentPostRequestDto requestDto, Long recruitmentPostId);
 
+  /**
+   * 채용공고 삭제
+   * @param recruitmentPostId
+   */
   void deleteRecruitmentPost(Long recruitmentPostId);
 
+  /**
+   * 채용공고 전체조회
+   * @return
+   */
   List<RecruitmentPostListResponseDto> getAllRecruitmentPost();
 
+  /**
+   * 채용공고 조건 검색
+   * @param keyword
+   * @return
+   */
   List<RecruitmentPostResponseDto> getSearchRecruitmentPost(String keyword);
+
+  /**
+   * 채용공고 단 건 조회
+   * @param recruitmentPostId
+   * @return
+   */
   RecruitmentPostDetailResponseDto getRecruitmentPost(Long recruitmentPostId);
 
+  /**
+   * 채용공고 찾기
+   * @param id
+   * @return
+   */
   RecruitmentPost findRecruitmentPost(Long id);
 }
